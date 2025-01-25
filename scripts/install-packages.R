a06a9db2b4cf74434c24a95224d45e533542ca8a
Lukas Burk <lukas@quantenbrot.de>
2025-01-25 12:29:18 +0100

    Built site for gh-pages

---
+++ scripts/install-packages.R
@@ -0,0 +1,16 @@
+# Installing pak to ease subsequent package installation
+install.packages("pak")
+
+# Installing main course packages using pak (faster, does caching)
+pak::pak(c(
+  "tidyverse",
+  "gapminder",
+  "gtsummary",
+  "ggstatsplot",
+  "rmarkdown",
+  "readxl",
+  "haven",
+  "readr",
+  "table1",
+  "ggsurvfit"
+))
